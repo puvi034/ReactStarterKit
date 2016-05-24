f1ceb9496474e2930c8505863a2ef87cdb4197cb
puvi034 <puvi034@gmail.com>
2016-05-23 22:17:08 -0700

    These two files have been set as an example template.

---
+++ App.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Puneeth T S on 5/23/16.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <h1>Hello World</h1>
+        )
+    }
+}
+
+ReactDOM.render(<App/>, document.getElementById("app"));
